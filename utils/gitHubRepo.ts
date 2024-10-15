1efe1fd3d6792bcf7bafd62f69b63a3acde5baf0
Sebastian Di Luzio <sebastian@diluz.io>
2024-10-15 23:14:03 +0200

    fix: eslint

--- utils/gitHubRepo.ts
+++ utils/gitHubRepo.ts
@@ -1,14 +1,14 @@
 export const gitHubProjectUrl = new URL(
-  "https://github.com/maybeanerd/celeryband"
+  'https://github.com/maybeanerd/celeryband',
 );
 
-export function getLinkToCommit(commitHash: string) {
+export function getLinkToCommit (commitHash: string) {
   const newUrl = new URL(gitHubProjectUrl);
   newUrl.pathname = `${gitHubProjectUrl.pathname}/commit/${commitHash}`;
   return newUrl;
 }
 
-export function getLinkToTag(tag: string) {
+export function getLinkToTag (tag: string) {
   const newUrl = new URL(gitHubProjectUrl);
   newUrl.pathname = `${gitHubProjectUrl.pathname}/releases/tag/${tag}`;
   return newUrl;
