90febc909d1a53cf09dc9bc1ac987d407d540c13
Sebastian Di Luzio <sebastian@diluz.io>
2024-10-15 23:28:37 +0200

    init nuxt (#2)
    * init
    
    * ci: use node v22
    
    * fix: eslint
    
    * chore: provide vscode config
    
    * Update renovate.json

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="flex flex-col min-h-full justify-between">
+      <div class="p-4 lg:p-8 w-screen overflow-x-hidden">
+        <slot />
+      </div>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ utils/gitHubRepo.ts
@@ -0,0 +1,15 @@
+export const gitHubProjectUrl = new URL(
+  'https://github.com/maybeanerd/celeryband',
+);
+
+export function getLinkToCommit (commitHash: string) {
+  const newUrl = new URL(gitHubProjectUrl);
+  newUrl.pathname = `${gitHubProjectUrl.pathname}/commit/${commitHash}`;
+  return newUrl;
+}
+
+export function getLinkToTag (tag: string) {
+  const newUrl = new URL(gitHubProjectUrl);
+  newUrl.pathname = `${gitHubProjectUrl.pathname}/releases/tag/${tag}`;
+  return newUrl;
+}
