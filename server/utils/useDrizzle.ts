4b92ad80ce484355ab16fd9f9a4db13afe32302e
Sebastian Di Luzio <sebidiluzio@gmail.com>
2024-10-24 22:17:55 +0200

    fix: store sqlite in location the docker compose expects

--- server/utils/useDrizzle.ts
+++ server/utils/useDrizzle.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import * as schema from '~/server/db/schema';
 
 export function useDrizzle () {
-  return { db: drizzle('celery.sqlite', { schema }) };
+  return { db: drizzle('./data/db.sqlite3', { schema }) };
 }
