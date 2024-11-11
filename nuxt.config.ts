16229540a4d20504c47a768702b4c5e9a93fe2eb
Sebastian Di Luzio <sebidiluzio@gmail.com>
2024-11-11 22:10:38 +0100

    feat: add nuxt-auth-utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ const { version } = packageJson;
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxtjs/i18n', '@vite-pwa/nuxt', '@nuxt/ui'],
+  modules: ['@nuxt/image', '@nuxtjs/i18n', '@vite-pwa/nuxt', '@nuxt/ui', 'nuxt-auth-utils'],
   pwa: {},
 
   nitro: {
