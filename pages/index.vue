9eda814c730301904b25007054966da447b59f3f
Sebastian Di Luzio <sebastian@diluz.io>
2024-11-14 22:59:00 +0100

    feat: add general auth support (#42)

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,55 @@
 <template>
-  <div>
+  <div class="flex flex-col gap-2">
     <h1>Celery Band</h1>
-    <br>
-    <UButton :loading="loading" @click="addUser">
-      Add user
+    <div v-if="loggedIn">
+      <UButton class="max-w-36" :loading="loading" @click="clear">
+        Log Out
+      </UButton>
+      <p>
+        Logged in as: {{ user }}
+      </p>
+      <p>
+        Session data: {{ session }}
+      </p>
+    </div>
+    <UButton v-else class="max-w-36" :loading="loading" @click="logIn">
+      Log In
     </UButton>
-    <br>
-    <br>
-    <p>users: {{ data }}</p>
+
+    <UButton class="max-w-36" @click="refresh">
+      Refresh
+    </UButton>
+
+    <p v-if="data">
+      users: {{ data }}
+    </p>
+    <p v-else>
+      error: {{ error }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { User } from '~/server/db/schemas/User.schema';
 
-const { data, refresh } = await useFetch<Array<User>>('/api/users', {
+const { loggedIn, user, session, fetch: fetchUserSession, clear } = useUserSession();
+
+const { data, error, refresh } = await useFetch<Array<User>>('/api/users', {
   lazy: true,
 });
 
 const loading = ref(false);
 
-const addUser = async () => {
+const logIn = async () => {
   loading.value = true;
-  await fetch('/api/users', {
+  await fetch('/api/login', {
     method: 'POST',
   });
 
   await refresh();
+  await fetchUserSession();
 
   loading.value = false;
 };
+
 </script>
