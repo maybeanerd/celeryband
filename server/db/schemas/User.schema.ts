25cd7bba6d7e42860eec5c67ec302110382b331b
Sebastian Di Luzio <sebastian@diluz.io>
2024-11-15 20:54:10 +0100

    Revert "tmp: add new column without migration to trigger failing pipeline"
    This reverts commit ddd446ac6786ac9f51477373577fb81bf2da92f4.

--- server/db/schemas/User.schema.ts
+++ server/db/schemas/User.schema.ts
@@ -3,7 +3,6 @@ import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const userSchema = sqliteTable('user', {
   id: text().primaryKey(), // TODO somehow generate UUIDs here, preferrably by default
   emailHash: text().unique().notNull(),
-  failingNewAttribute: text().notNull(),
 });
 
 export type User = typeof userSchema.$inferSelect;
