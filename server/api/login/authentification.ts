a294fb8bd3fdc85625549783b54e23640ba91838
Sebastian Di Luzio <sebidiluzio@gmail.com>
2024-11-22 18:46:03 +0100

    feat: store and reuse salt

--- server/api/login/authentification.ts
+++ server/api/login/authentification.ts
@@ -1,11 +1,28 @@
 import { randomBytes, scrypt } from 'crypto';
 import { eq } from 'drizzle-orm';
 import { loginTokenSchema } from '~/server/db/schemas/LoginToken.schema';
+import { obfuscationSaltSchema } from '~/server/db/schemas/ObfuscationSalt.schema';
 
-export function obfuscateEmail (email: string): Promise<string> {
-  // TODO generate a random salt once, and then reuse within server
-  // (this is necessary because emails need to always resolve to the same email again)
-  const salt = 'coolRandomSalt';
+// Creates and reuses a single shared salt.
+// This is necessary because emails need to always be obfuscated the same way to allow re-log-ins
+async function ensureObfuscationSalt (): Promise<string> {
+  const { db } = useDrizzle();
+  const existingSalt = await db.query.obfuscationSalt.findFirst();
+  if (existingSalt) {
+    return existingSalt.salt;
+  }
+
+  const salt = randomBytes(64).toString('hex');
+  const insertedRows = await db.insert(obfuscationSaltSchema).values({ salt }).returning();
+  const createdSalt = insertedRows.at(0);
+  if (!createdSalt) {
+    throw new Error('Was not able to create salt');
+  }
+  return createdSalt.salt;
+}
+
+export async function obfuscateEmail (email: string): Promise<string> {
+  const salt = await ensureObfuscationSalt();
 
   const obfuscatedEmail = new Promise<string>((resolve, reject) => {
     scrypt(email, salt, 64, (err, derivedKey) => {
