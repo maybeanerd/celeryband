d1cdbcdead517040adf324a038a98162b7f70870
Sebastian Di Luzio <sebastian@diluz.io>
2025-04-03 21:38:03 +0200

    feat: provide obfuscation salt in envvars

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,3 +1,2 @@
 export { userSchema as user } from './schemas/User.schema';
 export { loginTokenSchema as loginToken } from './schemas/LoginToken.schema';
-export { obfuscationSaltSchema as obfuscationSalt } from './schemas/ObfuscationSalt.schema';
--- server/db/schemas/ObfuscationSalt.schema.ts
+++
@@ -1,8 +0,0 @@
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-export const obfuscationSaltSchema = sqliteTable('obfuscationSalt', {
-  salt: text().primaryKey(),
-});
-
-export type ObfuscationSalt = typeof obfuscationSaltSchema.$inferSelect;
-export type ObfuscationSaltInsert = typeof obfuscationSaltSchema.$inferInsert;
