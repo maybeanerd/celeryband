56e15615f6d573557a2b357f22879a0ebcd419ce
Sebastian Di Luzio <sebidiluzio@gmail.com>
2025-04-17 23:53:14 +0200

    feat: add salary table

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,2 +1,3 @@
 export { userSchema as user } from './schemas/User.schema';
 export { loginTokenSchema as loginToken } from './schemas/LoginToken.schema';
+export { salarySchema as salary } from './schemas/salary.schema';
