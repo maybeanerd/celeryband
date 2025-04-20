11bf94fdb155c03e6f4b48a2799b924a18a730ef
Sebastian Di Luzio <sebidiluzio@gmail.com>
2025-04-20 23:40:14 +0200

    fix: dont round, instead include 2 decimals in percentages

--- server/src/modules/salary/salaries.ts
+++ server/src/modules/salary/salaries.ts
@@ -172,14 +172,14 @@ export async function getSalaryStatistics (userId: string) {
   const salaryAssessment = {
     overall: statisticsOfSameRoleAndSeniority
       ? {
-          average: Math.round(((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniority.average) / statisticsOfSameRoleAndSeniority.average) * 100),
-          median: Math.round(((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniority.median) / statisticsOfSameRoleAndSeniority.median) * 100),
+          average: (((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniority.average) / statisticsOfSameRoleAndSeniority.average) * 100).toFixed(2),
+          median: (((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniority.median) / statisticsOfSameRoleAndSeniority.median) * 100).toFixed(2),
         }
       : null,
     department: statisticsOfSameRoleAndSeniorityAndDepartment
       ? {
-          average: Math.round(((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniorityAndDepartment.average) / statisticsOfSameRoleAndSeniorityAndDepartment.average) * 100),
-          median: Math.round(((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniorityAndDepartment.median) / statisticsOfSameRoleAndSeniorityAndDepartment.median) * 100),
+          average: (((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniorityAndDepartment.average) / statisticsOfSameRoleAndSeniorityAndDepartment.average) * 100).toFixed(2),
+          median: (((userSalary.yearlyAmount - statisticsOfSameRoleAndSeniorityAndDepartment.median) / statisticsOfSameRoleAndSeniorityAndDepartment.median) * 100).toFixed(2),
         }
       : null,
   };
