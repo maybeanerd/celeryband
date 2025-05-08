b7fe03c436bdd218778b28c78e70d9484b1ee47e
Sebastian Di Luzio <sebastian@diluz.io>
2025-05-09 00:02:16 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import packageJson from './package.json';
 // When built in GitHub Actions, the commit hash is available in the environment, and gitCommitInfo won't find it.
 const commitHash = gitCommitInfo().shortHash ?? process.env.COMMIT_HASH;
 
-const { version } = packageJson;
+const { version } = packageJson; // TODO get version from git tag during container build
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
